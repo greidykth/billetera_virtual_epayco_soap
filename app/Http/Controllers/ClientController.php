cda8b4869f12ec4a9748700fe8cfd6f7d92fcd0a
Greidy Peña <greidykp@gmail.com>
2022-12-19 22:33:06 -0500

    Funcionalidades para registrar cliente y login

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,83 +3,148 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\Wallet;
+use App\Traits\SoapResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController
 {
+    use SoapResponseTrait;
+
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Create an instance of client, require dni, name, email, cellphone
+     * 
+     * @param int $dni
+     * @param string $name
+     * @param string $email
+     * @param int $cellphone
+     * @return object
      */
-    public function index()
+
+    public function registerClient($dni, $name, $email, $cellphone)
     {
-        //
+        $inputs = [
+            'dni' => $dni,
+            'name' => $name,
+            'email' => $email,
+            'cellphone' => $cellphone,
+        ];
+
+        $rules = [
+            'dni' => "required|unique:clients",
+            'name' => "required",
+            'email' => "required|unique:clients",
+            'cellphone' => "required|unique:clients",
+        ];
+
+        try {
+            $validation = Validator::make($inputs, $rules);
+
+            if ($validation->fails()) {
+                return $this->responseWhitData(
+                    false,
+                    "Datos faltantes o repetidos. Uno o varios campos son requeridos",
+                    422,
+                    (object)['error' => $validation->errors()->getMessages()]
+                );
+            }
+
+            $client = new Client();
+            $client->name = $name;
+            $client->dni = $dni;
+            $client->password = Hash::make($dni);
+            $client->email = $email;
+            $client->cellphone = $cellphone;
+            $client->save();
+            $token = $client->createToken('auth_token')->plainTextToken;
+
+            $wallet = new Wallet();
+            $wallet->balance = 0;
+            $wallet->client_id = $client->id;
+            $wallet->save();
+
+            return $this->responseWhitData(
+                true,
+                "Cliente registrado satisfactoriamente",
+                00,
+                [
+                    'token' => $token,
+                    'client' => (object)$client->toArray(),
+                    'saldo' => $client->wallet->balance,
+                ]
+            );
+        } catch (\Throwable $th) {
+            return $this->responseWhitData(
+                false,
+                "Algo ha fallado durante el registro del cliente",
+                422,
+                ['error' => $th]
+            );
+        }
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Login, require dni and cellphone
+     * 
+     * @param int $dni
+     * @param int $cellphone
+     * @return object
      */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function login($dni, $cellphone)
     {
-        //
-    }
+        $inputs = [
+            'dni' => $dni,
+            'cellphone' => $cellphone,
+        ];
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Client $client)
-    {
-        //
-    }
+        $rules = [
+            'dni' => "required",
+            'cellphone' => "required",
+        ];
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Client $client)
-    {
-        //
-    }
+        try {
+            $validation = Validator::make($inputs, $rules);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\Request  $request
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Client $client)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Client $client)
-    {
-        //
+            if ($validation->fails()) {
+                return $this->responseWhitData(
+                    false,
+                    "Datos faltantes o repetidos. Uno o varios campos son requeridos",
+                    422,
+                    (object)['error' => $validation->errors()->getMessages()]
+                );
+            }
+            $client = Client::where('dni', $dni)->where('cellphone', $cellphone)->first();
+
+            if (is_null($client)) {
+                return $this->responseNoData(
+                    false,
+                    "Las credenciales proporcionadas son inválidas",
+                    401
+                );
+            }
+            $token = $client->createToken('auth_token')->plainTextToken;
+
+            return $this->responseWhitData(
+                true,
+                "Cliente autenticado. Bienvenido(a) a tu billetera virtual",
+                00,
+                (object)[
+                    'token' => $token,
+                    'client' => (object)$client->toArray(),
+                    'saldo' => $client->wallet->balance
+                ],
+            );
+        } catch (\Throwable $th) {
+            return $this->responseWhitData(
+                false,
+                "Algo ha fallado durante el login",
+                422,
+                ['error' => $th]
+            );
+        }
     }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'dni', 'name', 'email', 'cellphone',
+    ];
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+    }
+
 }
